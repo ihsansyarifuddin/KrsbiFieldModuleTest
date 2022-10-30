8aeef430866ffae259eef1ce5b2257b2e576665a
Ihsan Syarifuddin <ihsansyarifuddin001@gmail.com>
2022-10-30 23:32:38 +0700

    robot movement example

--- KrsbiFieldModuleTest/Form1.Designer.cs
+++ KrsbiFieldModuleTest/Form1.Designer.cs
@@ -31,6 +31,16 @@
             this.fieldLoader = new KrsbiFieldModule.FieldLoader();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.xTextbox = new System.Windows.Forms.TextBox();
+            this.yTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // fieldLoader
@@ -62,17 +72,110 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.hTextbox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.yTextbox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.xTextbox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(618, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 161);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Movement Test";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Robot 1",
+            "Robot 2",
+            "Robot 3",
+            "Robot 4",
+            "Robot 5",
+            "Robot G"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pos. X";
+            // 
+            // xTextbox
+            // 
+            this.xTextbox.Location = new System.Drawing.Point(52, 48);
+            this.xTextbox.Name = "xTextbox";
+            this.xTextbox.Size = new System.Drawing.Size(100, 20);
+            this.xTextbox.TabIndex = 2;
+            // 
+            // yTextbox
+            // 
+            this.yTextbox.Location = new System.Drawing.Point(52, 74);
+            this.yTextbox.Name = "yTextbox";
+            this.yTextbox.Size = new System.Drawing.Size(100, 20);
+            this.yTextbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pos. Y";
+            // 
+            // hTextbox
+            // 
+            this.hTextbox.Location = new System.Drawing.Point(52, 100);
+            this.hTextbox.Name = "hTextbox";
+            this.hTextbox.Size = new System.Drawing.Size(100, 20);
+            this.hTextbox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Heading";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 469);
+            this.ClientSize = new System.Drawing.Size(879, 469);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.fieldLoader);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +185,15 @@
         private KrsbiFieldModule.FieldLoader fieldLoader;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox hTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox yTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox xTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- KrsbiFieldModuleTest/Form1.cs
+++ KrsbiFieldModuleTest/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KrsbiFieldModule.Singleton.Robot;
+using KrsbiFieldModule.Utility;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,13 @@ namespace KrsbiFieldModuleTest
 {
     public partial class Form1 : Form
     {
+        private Robot1 robot1;
+        private Robot2 robot2;
+        private Robot3 robot3;
+        private Robot4 robot4;
+        private Robot5 robot5;
+        private RobotG robotG;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,13 +33,68 @@ namespace KrsbiFieldModuleTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            robot1 = Robot1.GetInstance();
+            robot2 = Robot2.GetInstance();
+            robot3 = Robot3.GetInstance();
+            robot4 = Robot4.GetInstance();
+            robot5 = Robot5.GetInstance();
+            robotG = RobotG.GetInstance();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();
             form2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var robot = comboBox1.Text;
+                var x = int.Parse(xTextbox.Text);
+                var y = int.Parse(yTextbox.Text);
+                var h = double.Parse(hTextbox.Text);
+                var fieldConfig = fieldLoader.GetFieldConfig();
+                KeyValuePair<string, object> isEnable;
+                switch (robot)
+                {
+                    case "Robot 1":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobot1").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if(bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robot1, new Point(x, y), h);
+                        break;
+                    case "Robot 2":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobot2").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if (bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robot2, new Point(x, y), h);
+                        break;
+                    case "Robot 3":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobot3").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if (bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robot3, new Point(x, y), h);
+                        break;
+                    case "Robot 4":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobot4").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if (bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robot4, new Point(x, y), h);
+                        break;
+                    case "Robot 5":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobot5").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if (bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robot5, new Point(x, y), h);
+                        break;
+                    case "Robot G":
+                        isEnable = fieldConfig.Where(item => item.Key == "teamRobotG").FirstOrDefault();
+                        if (isEnable.Value == null) break;
+                        if (bool.Parse(isEnable.Value.ToString()) == true) RobotHandler.MoveRobotToCoordinate(robotG, new Point(x, y), h);
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
