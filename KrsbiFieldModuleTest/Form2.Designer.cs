9993d3517cfb8d5e27748a3b5a4439463c93cc4d
Ihsan Syarifuddin <ihsansyarifuddin001@gmail.com>
2022-10-30 21:25:50 +0700

    Add project files.

---
+++ KrsbiFieldModuleTest/Form2.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace KrsbiFieldModuleTest
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fieldConfigurator1 = new KrsbiFieldModule.FieldConfigurator();
+            this.SuspendLayout();
+            // 
+            // fieldConfigurator1
+            // 
+            this.fieldConfigurator1.Location = new System.Drawing.Point(13, 13);
+            this.fieldConfigurator1.Name = "fieldConfigurator1";
+            this.fieldConfigurator1.Size = new System.Drawing.Size(740, 449);
+            this.fieldConfigurator1.TabIndex = 0;
+            this.fieldConfigurator1.Load += new System.EventHandler(this.fieldConfigurator1_Load);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 472);
+            this.Controls.Add(this.fieldConfigurator1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private KrsbiFieldModule.FieldConfigurator fieldConfigurator1;
+    }
+}
---
+++ KrsbiFieldModuleTest/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KrsbiFieldModuleTest
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void fieldConfigurator1_Load(object sender, EventArgs e)
+        {
+            fieldConfigurator1.SaveResponse += FieldConfigurator1_SaveResponse;
+        }
+
+        private void FieldConfigurator1_SaveResponse(object sender, KrsbiFieldModule.EventObject.ConfigurationEventArgs e)
+        {
+            if (e.IsSuccess) Hide();
+            else MessageBox.Show(e.Message);
+        }
+    }
+}
